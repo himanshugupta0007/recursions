0427a1fd427c87825f347bdc823e6b04893eed1e
Himanshu <himanshugupta0007@gmail.com>
2021-12-20 23:44:00 +0530

    Adding Source

---
+++ src/com/easy/problems/SumOfNNumbers.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.easy.problems;
+
+/**
+ * @author himanshugupta
+ *
+ */
+public class SumOfNNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+
+	}
+
+}
